304ecbc1d6802c1f40a8a712f385fdf5d090e444
Himanshu Kumar <118829051+Himanshu12211967@users.noreply.github.com>
2025-04-29 14:25:04 +0530

    Time: 0 ms (100%), Space: 11.5 MB (34.68%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -4,23 +4,17 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         
         reverse(digits.begin(),digits.end());
-
-        int i=0;
         int carry = 1;
 
-        while(i < digits.size()){
-
-            int num = digits[i];
-            int sum = num + carry;
+        for(int i=0; i<digits.size(); i++){
+            int sum = carry + digits[i];
             carry = sum/10;
-            sum = sum%10;
+            sum %= 10;
             digits[i] = sum;
-            i++;
         }
 
-        if(carry != 0) digits.push_back(carry);
+        if(carry) digits.push_back(carry);
         reverse(digits.begin(),digits.end());
         return digits;
-
     }
 };
