c6a6b740cc6f811251fee68a56465ce1dc08c037
Himanshu Kumar <118829051+Himanshu12211967@users.noreply.github.com>
2025-06-07 13:56:26 +0530

    Time: 19 ms (21.58%), Space: 25.7 MB (16.94%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
 
-    bool solve(string s1, string s2, string s3,int i,int j,int k,vector<vector<vector<int>>> &dp){
+    bool solve(string s1, string s2, string s3,int i,int j,int k,vector<vector<int>> &dp){
 
         if(k >= s3.length()) return true;
-        if(dp[i][j][k] != -1) return dp[i][j][k];
+        if(dp[i][j] != -1) return dp[i][j];
 
         if(i < s1.length() && s1[i] == s3[k]){
             if(solve(s1,s2,s3,i+1,j,k+1,dp)) return true;
@@ -12,14 +12,14 @@ public:
         if(j < s2.length() && s2[j] == s3[k]){
             if(solve(s1,s2,s3,i,j+1,k+1,dp)) return true;
         }
-        return dp[i][j][k] = false;
+        return dp[i][j] = false;
 
     }
 
     bool isInterleave(string s1, string s2, string s3) {
         
         if(s1.length()+s2.length() != s3.length()) return false;
-        vector<vector<vector<int>>> dp(s1.length()+1,vector<vector<int>>(s2.length()+1,vector<int>(s3.length(),-1)));
+        vector<vector<int>> dp(s1.length()+1,vector<int>(s3.length(),-1));
         return solve(s1,s2,s3,0,0,0,dp);
 
     }
