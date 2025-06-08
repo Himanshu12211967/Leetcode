864f8aa502aac7d2f1a61218466a512f93db2867
Himanshu Kumar <118829051+Himanshu12211967@users.noreply.github.com>
2025-06-08 12:39:35 +0530

    Time: 601 ms (5.04%), Space: 277.3 MB (5.36%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,26 +1,55 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(3, vector<int>(2, 0)));
 
-        for (int idx = n-1; idx >= 0; idx--) {
-            for (int max_transaction = 0; max_transaction <= 2; max_transaction++) {
-                for (int canwebuy = 0; canwebuy <= 1; canwebuy++) {
-                    int ch1 = 0, ch2 = 0, ch3 = 0;
-                    if (canwebuy == 1 && max_transaction != 0) {
+    int solve(vector<int>& prices,int idx,int max_transaction,int canwebuy,vector<vector<vector<int>>> &dp){
+
+        if(idx == prices.size()) return 0;
+        if(dp[idx][max_transaction][canwebuy] != -1) return dp[idx][max_transaction][canwebuy];
+
+        int ch1 = 0,ch2 = 0,ch3 = 0;
+
+        if(canwebuy == 1 && max_transaction != 0){
+            ch1 = -prices[idx] + solve(prices,idx+1,max_transaction-1,0,dp);
+        }
+        else if(canwebuy == 0){
+            ch2 = prices[idx] + solve(prices,idx+1,max_transaction,1,dp);
+        }
+        ch3 = solve(prices,idx+1,max_transaction,canwebuy,dp);
+
+        return dp[idx][max_transaction][canwebuy] = max({ch1,ch2,ch3});
+
+    }
+
+    int solve2(vector<int>& prices){
+
+        vector<vector<vector<int>>> dp(prices.size()+1,vector<vector<int>>(3,vector<int>(2,0)));
+
+        for(int idx=prices.size()-1; idx>=0; idx--){
+            for(int max_transaction=0; max_transaction<=2; max_transaction++){
+                for(int canwebuy=0; canwebuy<=1; canwebuy++){
+                    int ch1 = 0,ch2 = 0,ch3 = 0;
+
+                    if(canwebuy == 1 && max_transaction != 0){
                         ch1 = -prices[idx] + dp[idx+1][max_transaction-1][0];
                     }
-                    else if (canwebuy == 0) {
+                    else if(canwebuy == 0){
                         ch2 = prices[idx] + dp[idx+1][max_transaction][1];
                     }
                     ch3 = dp[idx+1][max_transaction][canwebuy];
 
-                    dp[idx][max_transaction][canwebuy] = max({ch1, ch2, ch3});
+                    dp[idx][max_transaction][canwebuy] = max({ch1,ch2,ch3});
                 }
             }
         }
 
         return dp[0][2][1];
+    }
+
+    int maxProfit(vector<int>& prices) {
+        
+        //vector<vector<vector<int>>> dp(prices.size()+1,vector<vector<int>>(3,vector<int>(3,-1)));
+        //return solve(prices,0,2,1,dp);
+        return solve2(prices);
+
     }
 };
