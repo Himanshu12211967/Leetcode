ebc64a21f3ff2388088d100dec5b605792276485
Himanshu Kumar <118829051+Himanshu12211967@users.noreply.github.com>
2025-06-08 10:27:16 +0530

    Time: 16 ms (3.89%), Space: 11.3 MB (95.57%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-
-    int solve(int row,int col,int n,int m,vector<vector<int>>& obstacleGrid){
-
-        if(row >= 0 && col >= 0 && row < n && col < m && obstacleGrid[row][col] != -1) return obstacleGrid[row][col];
-        return 0;
- 
-    }
-
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         
-        //return 0;
-        int n = obstacleGrid.size();
-        int m = obstacleGrid[0].size();
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        if(obstacleGrid[m-1][n-1] == 1 || obstacleGrid[0][0] == 1) return 0;
 
-        if(obstacleGrid[n-1][m-1] == 1 || obstacleGrid[0][0]) return 0;
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
 
-        for(int i=n-1; i>=0; i--){
-            for(int j=m-1; j>=0; j--){
-                if(i == n-1 && j == m-1){
+                if(i == m-1 && j == n-1){
                     obstacleGrid[i][j] = 1;
                     continue;
                 }
 
                 if(obstacleGrid[i][j] == 1){
-                    obstacleGrid[i][j]=-1;
+                    obstacleGrid[i][j] = INT_MIN;
                     continue;
                 }
 
-                long long right = solve(i,j+1,n,m,obstacleGrid);
-                long long bottom = solve(i+1,j,n,m,obstacleGrid);
-                obstacleGrid[i][j] = (right + bottom)%2000000000;
+                long long int right = 0,down = 0;
 
+                if(j+1 < n && obstacleGrid[i][j+1] != INT_MIN) right = obstacleGrid[i][j+1];
+                if(i+1 < m && obstacleGrid[i+1][j] != INT_MIN) down = obstacleGrid[i+1][j];
+
+                obstacleGrid[i][j] = (down + right)%2000000000;
             }
+        }
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                cout<<obstacleGrid[i][j]<<" ";
+            }
+            cout<<endl;
         }
 
         return obstacleGrid[0][0];
