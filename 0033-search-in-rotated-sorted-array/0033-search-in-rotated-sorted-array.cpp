d372a200f450b895c4af01fdb46940f82df85dd7
Himanshu Kumar <118829051+Himanshu12211967@users.noreply.github.com>
2025-06-09 07:41:28 +0530

    Time: 0 ms (100%), Space: 15.3 MB (33.03%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,44 +1,46 @@
 class Solution {
 public:
 
-    int getpivot(vector<int> &nums){
+    int binarysearch(vector<int> &nums,int st,int end,int target){
 
-        int st = 0;
-        int end = nums.size()-1;
+        while(st <= end){
+            int mid = st+(end-st)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) end = mid-1;
+            else st = mid+1;
+        }
+
+        return -1;
+    }
+
+    int peakelement(vector<int> &nums){
+
+        int st = 0,end = nums.size()-1;
 
         while(st < end){
-            int mid = st + (end-st)/2;
+
+            int mid = st+(end-st)/2;
             if(nums[mid] >= nums[0]) st = mid+1;
             else end = mid;
         }
 
         return st;
+        
     }
 
-    int binarysearch(vector<int> &nums, int st, int end, int target){
-
-        while(st <= end){
-            int mid = st + (end-st)/2;
-            if(nums[mid] == target) return mid;
-            else if(nums[mid] < target) st = mid+1;
-            else end = mid-1;
-        }
-        return -1;
-    }
-
-    int search(vector<int>& nums, int key) {
-
+    int search(vector<int>& nums, int target) {
+        
         if(nums.size() == 1){
-            if(nums[0] == key) return 0;
+            if(nums[0] == target) return 0;
             return -1;
         }
-
-        int pivot = getpivot(nums);
-        cout<<pivot;
-        if(key >= nums[0] && key <= nums[pivot-1]){
-            return binarysearch(nums,0,pivot-1,key);
-        }
-        return binarysearch(nums,pivot,nums.size()-1,key);
+        
+        int peak = peakelement(nums);
+        cout<<peak;
+        if(target >= nums[0] && target <= nums[peak-1]){
+            return binarysearch(nums,0,peak-1,target);
+        } 
+        return binarysearch(nums,peak,nums.size()-1,target);
 
     }
 };
