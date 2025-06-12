b1945d9cf63f3192faa1b499cdcb2c688f6b6796
Himanshu Kumar <118829051+Himanshu12211967@users.noreply.github.com>
2025-06-12 14:43:36 +0530

    Time: 3 ms (8.18%), Space: 11.5 MB (5.56%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
 
-    int solve(vector<vector<int>>& board,int x,int y){
+    int solve(vector<vector<int>> board,int x,int y){
 
         int dx[] = {0,0,1,-1,-1,-1,1,1};
         int dy[] = {1,-1,0,0,-1,1,-1,1};
 
-        int dead = 0;
         int live = 0;
 
         for(int i=0; i<8; i++){
             int nx = dx[i] + x;
             int ny = dy[i] + y;
-            if(nx >= 0 &&  ny >= 0 && nx < board.size() && ny < board[0].size() && board[nx][ny] == 1) live++;
-            if(nx >= 0 &&  ny >= 0 && nx < board.size() && ny < board[0].size() && board[nx][ny] == 0) dead++;
+            if(nx >= 0 &&  ny >= 0 && nx < board.size() && ny < board[0].size() && (board[nx][ny] == -1 || board[nx][ny] == 1)) live++;
+            
         }
 
-        if(board[x][y] == 0 && live == 3) return 1;
-        if(board[x][y] == 1 && (live == 2 || live == 3)) return 1;
-        return 0;
+        if(board[x][y] == 0 && live == 3) return 2;
+        if(board[x][y] == 1 && (live < 2 || live > 3)) return -1;
+        return board[x][y];
     }
 
     
@@ -31,13 +30,13 @@ public:
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
                 int val = solve(board,i,j);
-                matrix[i][j] = val;
+                board[i][j] = val;
             }
         }
 
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
-                board[i][j] = matrix[i][j];
+                board[i][j] = (board[i][j] <= 0) ? 0 : 1;
             }
         }
     }
