482e0d3844cea9803aa460253763d3321249d156
Himanshu Kumar <118829051+Himanshu12211967@users.noreply.github.com>
2025-06-30 09:45:01 +0530

    Time: 271 ms (29.83%), Space: 62.2 MB (8.14%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -9,9 +9,21 @@ public:
         return solve(nums,target,idx+1,sum+nums[idx]) + solve(nums,target,idx+1,sum-nums[idx]);
     }
 
+    int tabulation(vector<int>& nums, int target, int idx, int sum, map<pair<int,int>,int> &dp){
+
+        if(idx == nums.size() && target == sum) return 1;
+        if(idx == nums.size()) return 0;
+        if(dp.find({idx,sum}) != dp.end()) return dp[{idx,sum}];
+        
+        int opt1 = tabulation(nums,target,idx+1,sum+nums[idx],dp);
+        int opt2 = tabulation(nums,target,idx+1,sum-nums[idx],dp);
+        return dp[{idx,sum}] = opt1 + opt2;
+    }
+
     int findTargetSumWays(vector<int>& nums, int target) {
         
-        return solve(nums,target,0,0);
+        map<pair<int,int>,int> dp;
+        return tabulation(nums,target,0,0,dp);
 
     }
 };
