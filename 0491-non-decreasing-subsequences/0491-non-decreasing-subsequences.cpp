b23bda889f0bc262fce6b9425c5622844800fb73
Himanshu Kumar <118829051+Himanshu12211967@users.noreply.github.com>
2025-07-02 09:45:42 +0530

    Time: 80 ms (22.97%), Space: 77.3 MB (14.04%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    void solve(int idx, vector<vector<int>> &ans,vector<int>& nums,vector<int> temp){
+
+        if(idx == nums.size()){
+            if(temp.size() >= 2)
+            ans.push_back(temp);
+            return;
+        }
+
+        if(idx > 0 && temp.size() > 0 && temp[temp.size()-1] == nums[idx]){
+            if(temp.empty() ||temp.size() > 0 && temp.back() <= nums[idx]){
+                temp.push_back(nums[idx]);
+                solve(idx+1,ans,nums,temp);
+                temp.pop_back();
+            }
+        }
+        else{
+            if(temp.empty() || temp.size() > 0 && temp.back() <= nums[idx]){
+                temp.push_back(nums[idx]);
+                solve(idx+1,ans,nums,temp);
+                temp.pop_back();
+            }
+            solve(idx+1,ans,nums,temp);
+        }
+
+    }
+
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        
+        vector<vector<int>> ans;
+        vector<int> temp;
+        solve(0,ans,nums,temp);
+        return ans;
+    }
+};
