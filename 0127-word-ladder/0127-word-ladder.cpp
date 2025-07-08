127b6119af01fc539ead6edd9acf4bc1e4fd4591
Himanshu Kumar <118829051+Himanshu12211967@users.noreply.github.com>
2025-07-08 10:37:10 +0530

    Time: 303 ms (36.42%), Space: 21.3 MB (37.41%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -3,42 +3,41 @@ public:
 
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         
-        int len = INT_MAX;
+
         queue<pair<string,int>> q;
         q.push({beginWord,1});
-
-        set<string> m;
+        set<string> st;
 
         for(int i=0; i<wordList.size(); i++){
-            m.insert(wordList[i]);
+            st.insert(wordList[i]);
         }
 
-        m.erase(beginWord);
+        if(st.find(endWord) == st.end()) return 0;
+
+        st.erase(beginWord);
 
         while(!q.empty()){
 
             string word = q.front().first;
-            int chainlen = q.front().second;
+            int len = q.front().second;
             q.pop();
 
-            if(word == endWord){
-                len = min(len,chainlen);
-            }
+            if(word == endWord) return len;
 
             for(int i=0; i<word.length(); i++){
                 char ori = word[i];
                 for(char ch='a'; ch<='z'; ch++){
                     word[i] = ch;
-                    if(m.find(word) != m.end()){
-                        q.push({word,chainlen+1});
-                        m.erase(word);
+                    if(st.find(word) != st.end()){
+                        q.push({word,len+1});
+                        st.erase(word);
                     }
                 }
                 word[i] = ori;
             }
+
         }
 
-        if(len == INT_MAX) return 0;
-        return len;
+        return 0;
     }
 };
