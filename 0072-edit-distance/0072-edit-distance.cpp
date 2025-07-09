58bfdd13890c832fda56d2493aa458d537ac58fe
Himanshu Kumar <118829051+Himanshu12211967@users.noreply.github.com>
2025-07-09 10:45:13 +0530

    Time: 7 ms (59.38%), Space: 13.2 MB (45.59%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
 
-    int solve(string &a,string &b,int i,int j,vector<vector<int>> &dp){
+    int tabulation(string &word1, string &word2){
 
-        if(i == a.length() && j < b.length()) return b.length()-j;
-        if(i < a.length() && j == b.length()) return a.length()-i;
-        if(i == a.length() && j == b.length()) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
+        vector<vector<int>> dp(word1.length()+1,vector<int>(word2.length()+1,0));
 
-        if(a[i] == b[j]){
-            return dp[i][j] = solve(a,b,i+1,j+1,dp);
+        int n = word1.length();
+        int m = word2.length();
+
+        for (int i = 0; i <= n; i++) dp[i][m] = n - i;  // Remaining deletions
+        for (int j = 0; j <= m; j++) dp[n][j] = m - j;  // Remaining insertions
+
+        for(int i=word1.length()-1; i>=0; i--){
+            for(int j=word2.length()-1; j>=0; j--){
+                if(word1[i] == word2[j]){
+                    dp[i][j] = dp[i+1][j+1];
+                }
+                else{
+                    int opt1 = 1 + dp[i+1][j+1];
+                    int opt2 = 1 + dp[i+1][j];
+                    int opt3 = 1 + dp[i][j+1];
+                    dp[i][j] = min({opt1,opt2,opt3});
+                }
+            }
         }
-        else{
-            int ch1 = 1 + solve(a,b,i,j+1,dp);
-            int ch2 = 1 + solve(a,b,i+1,j,dp);
-            int ch3 = 1 + solve(a,b,i+1,j+1,dp);
-            return dp[i][j] = min({ch1,ch2,ch3});
-        }
 
+        return dp[0][0];
     }
 
     int minDistance(string word1, string word2) {
-        
-        vector<vector<int>> dp(word1.length()+1,vector<int>(word2.length(),-1));
-        return solve(word1,word2,0,0,dp);
-
+        return tabulation(word1,word2);
     }
 };
