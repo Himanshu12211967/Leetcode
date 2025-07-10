0d6d551c1a461b775e4c36bcd3615d35c2593ede
Himanshu Kumar <118829051+Himanshu12211967@users.noreply.github.com>
2025-07-10 09:03:29 +0530

    Time: 303 ms (18.72%), Space: 291.8 MB (11.13%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -2,27 +2,29 @@ class Solution {
 public:
 
     string longestPalindrome(string s) {
+        
+        vector<vector<int>> dp(s.length(),vector<int>(s.length(),0));
+        int start = 0;
+        int min_len = INT_MIN;
 
-        vector<vector<bool>> dp(s.length(),vector<bool>(s.length(),false));
-        string lps = "";
-        int max_len = 0;
-        int start_index = 0;
-
-        for(int col=0; col<s.length(); ++col){
-            for(int row=0; row<=col; ++row){
-                if(row == col) dp[row][col] = true;
-                else if(s[row] == s[col] && (col-row == 1 || dp[row+1][col-1] == true)) dp[row][col] = true;
-                else dp[row][col] = false;
-
-                //updating the string index and length
-                if(dp[row][col] == true && max_len < col-row+1){
-                    max_len = col-row+1;
-                    start_index = row;
+        for(int col=0; col<s.length(); col++){
+            for(int row=0; row<=col; row++){
+                if(row == col) dp[row][col] = 1;
+                else if(s[row] == s[col] && (dp[row+1][col-1] == 1 || abs(col-row == 1))){
+                    dp[row][col] = 1;
                 }
-                
+                else dp[row][col] = 0;
+
+                if(dp[row][col] == 1 && min_len < col-row+1){
+                    min_len = col-row+1;
+                    start = row;
+                }
             }
         }
 
-        return s.substr(start_index,max_len);
+        if(min_len == INT_MIN) return "";
+        string ans = s.substr(start,min_len);
+        return ans;
+
     }
 };
